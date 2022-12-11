ea3220987f150f92fb55adfc246eb2125ceadb94
George Georgalis <george@galis.org>
2022-12-10 20:51:30 -0800

    workaround OSX Ventura rsync problem 254383328

--- know/music/.index.sh
+++ know/music/.index.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# render potential index changes to each declared volume
+# render potential index changes to each declared volume, or default set
 
 set -e
 
@@ -29,8 +29,8 @@ EOF
 while IFS= read a ; do
     validfn $a && true || { echo "$0 : validfn error : $a" 1>&2 ; dep_help_sub ; exit 1 ;}
     done <<EOF
-ckstat 466ad7a4 000003a8
-ckstatsum 728f2e3f 000003fe
+ckstat dbe14a8e 000003ae
+ckstatsum ddf65d49 00000413
 formfile 68be3ca9 00000f51
 formfilestats fa92ede0 000004dc
 revargs 5db3f9bb 000000a7
@@ -40,20 +40,12 @@ EOF
 ps | grep -E "^[ ]*$$" | grep -q bash   || chkexit "$0 : Not bash"
 test -d "$links/"                       || chkexit "$0 : not a directory links='$links'"
 
-cat >/dev/null <<EOF
-1. The first volume is [5fb3-deja-muse](https://github.com/georgalis/pub/blob/master/know/music/5fb3-deja-muse.md)
-1. The second volume is [660e-requeues](https://github.com/georgalis/pub/blob/master/know/music/660e-requeues.list)
-1. The third volume is [5deb-melody-royal](https://github.com/georgalis/pub/blob/master/know/music/5deb-melody-royal.list)
-1. The fourth volume is [5d50-kindle-class](https://github.com/georgalis/pub/blob/master/know/music/5d50-kindle-class.list)
-1. A fifth volume [6344-Ithica](https://github.com/georgalis/pub/blob/master/know/music/6344-Ithica.list)
-EOF
-
 f="$0"
 infile="${0##*/}"                                              # infile  == basename 0
 expr "$f" : ".*/" >/dev/null && inpath="${f%/*}" || inpath="." # inpath  ==  dirname 0
 infilep="$(cd "${inpath}" ; pwd -P)/${infile}"                 # infilep == realpath f
 
-gen_index () {
+gen_index () { # for "$links/$name/"
     mkdir -p html
 
     chktrue "${name}.list"
@@ -69,13 +61,16 @@ gen_index () {
     touch -r "$links/$name/" "${name}.list"
 
     chktrue ${name}.tab
+    local j="/tmp/${FUNCNAME}-$$/second"
+    mkdir -p "$j"
     cat ${name}.list | while IFS= read a ; do
-        b="$(formfile "$a" | sed '/^#/d')"
+        b="$(cd "$j" ; formfile "$a" | sed '/^#/d')"
         sed -e 's/   .*_^/ _^/' -e "s/'//" -e 's/^\(.*\)\( _^.*\)/\2 \1/' -e 's/^\(.*\),\([^ ]*\)/\2 \1 /' <<<"$b" \
             | awk '{printf "%-85s %-18s",$1,$2;$1="";$2="";$3="";print}'
         spin2
         done | sort -f >${name}.tab
     spin2 0
+    rm -rf "/tmp/${FUNCNAME}-$$/"
     touch -r "${name}.list" ${name}.tab
 
     chktrue ${name}.stat.time
@@ -89,8 +84,8 @@ gen_index () {
 
     chktrue ${name}.ckstat
     cat ${name}.list | while IFS= read a ; do
-      # ckstat    $links/$name/$a | awk -v f="${a##*,}" '{printf ". % 8s % 8s %s %s\n",$3,$4,f}'
-        ckstatsum $links/$name/$a | awk -v f="${a##*,}" '{printf ". % 8s % 8s %s %s\n",$2,$3,$4,f}'
+        ckstat    $links/$name/$a | awk -v f="${a##*,}" '{printf ". % 8s % 8s %s %s\n",$2,$3,$4,f}'
+      # ckstatsum $links/$name/$a | awk -v f="${a##*,}" '{printf ". % 8s % 8s %s %s\n",$2,$3,$4,f}'
         spin2
         done >${name}.ckstat
     spin2 0
@@ -140,9 +135,23 @@ gen_index () {
 
 check_do_index () { # bypass unchanged
     [ -d "$links/$name/" ] || { chkerr "$0 : not a directory '$links/$name/'" ; return 1 ;}
-    [ -e "${name}.list" ] && [ "${name}.list" -nt "$links/$name/" ] && { chktrue "No change, skipping $name" ; return 0 ;}
+    [ -e "${name}.list" ] && expr "$(ckstat "${name}.list" | awk '{print $5}' )" '=' "$(ckstat "$links/$name/" | awk '{print $5}' )" >/dev/null \
+        && { chktrue "No change, skipping $name" ; return 0 ;} || true # ie return if no change or continue to gen_index
     gen_index
     } # check_do_index
+
+touch_kind_rsync () {
+    mount | grep -q /Volumes/CURATE || { chkwrn "$0 : no /Volumes/CURATE for kind/$name" ; return 1 ;}
+    mkdir -p "/Volumes/CURATE/kind/$name" || { chkwrn "$0 : cannot create /Volumes/CURATE/kind/$name" ; return 1 ;}
+    # Fixup OSX Ventura rsync problem
+    # https://discussions.apple.com/thread/254383328
+    # https://github.com/WayneD/rsync/issues/412
+    cd "$links/0/kind/$name/" \
+      && find . -type f -maxdepth 1 -name \*\.mp3 | while IFS= read a ; do
+        touch -r "$a" "/Volumes/CURATE/kind/${name}/$a"
+        done
+    rsync -aPi --delete --delete-excluded --modify-window=1 --include='*mp3' --exclude='*' ./ "/Volumes/CURATE/kind/$name/"
+    }
 
 # main
 
@@ -155,8 +164,31 @@ volumes="
 6350-forte-flute
 "
 
+volumes="
+5fb3-deja-muse    https://github.com/georgalis/pub/blob/master/know/music/5fb3-deja-muse.md
+660e-requeues     https://github.com/georgalis/pub/blob/master/know/music/660e-requeues.list
+5deb-melody-royal https://github.com/georgalis/pub/blob/master/know/music/5deb-melody-royal.list
+5d50-kindle-class https://github.com/georgalis/pub/blob/master/know/music/5d50-kindle-class.list
+6344-Ithica       https://github.com/georgalis/pub/blob/master/know/music/6344-Ithica.list
+6350-forte-flute  https://github.com/georgalis/pub/blob/master/know/music/6350-forte-flute.list
+"
+
+vols="$(awk '!/^$/ {print $1}' <<<"$volumes")"
+
+echo "Volumes:"
+awk '!/^$/' <<<"$volumes" | awk '{print NR,$0}'
+
 cd "$inpath"
 
-[ "$*" ] && { for name in $* ; do [ -d "$links/$name/" ] && gen_index ; done ; exit $? ;} \
-         || { for name in $volumes ; do check_do_index ; done ; exit $? ;}
+# if the first arg is sync, do that after gen_index
+[ "$1" = "sync" ] && { _sync=y ; shift ;} || { _sync=n ;;}
 
+# always read args
+[ "$*" ] && { for name in $* ; do [ -d "$links/$name/" ] && gen_index ; done ;} \
+         || { for name in $vols ; do check_do_index ; done ;}
+
+[ "$_sync" = "y" ] && { # if first arg was sync, touch_kind_rsync remaining args, or all vols
+    [ "$*" ] && { for name in $* ; do [ -d "$links/$name/" -a -d "$links/0/kind/$name/" ] && touch_kind_rsync ; done ; exit $? ;} \
+             || { for name in $vols ; do touch_kind_rsync ; done ; exit $? ;} ;}
+
+exit 0
