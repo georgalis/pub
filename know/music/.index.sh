85323bebd6a0bb955af5444f25302b122825417d
George Georgalis <george@galis.org>
2023-01-15 15:50:11 -0800

    dev refresh OSX workaround

--- know/music/.index.sh
+++ know/music/.index.sh
@@ -17,22 +17,22 @@ while IFS= read a ; do
 devnul 216e1370 0000001d
 stderr 7ccc5704 00000037
 chkstd ee4aa465 00000032
-chkwrn 18c46093 0000005e
-logwrn e5806086 00000061
-chkerr 57d3ff82 0000005f
-logerr ffddd972 00000062
-chktrue 845489dd 00000064
-chkexit 8b52b10f 0000005e
-logexit e0f87299 00000061
+chkwrn 2683d3d3 0000005c
+logwrn f279f00e 0000005f
+chkerr 4f18299d 0000005b
+logerr 2db98372 0000005e
+chktrue f37189b7 00000060
+chkexit e6d9b430 0000005a
+logexit 235b98c9 0000005d
 validfn c268584c 00000441
 EOF
 
 while IFS= read a ; do
     validfn $a && true || { echo "$0 : validfn error : $a" 1>&2 ; dep_help_sub ; exit 1 ;}
     done <<EOF
-ckstat dbe14a8e 000003ae
+ckstat 4fe96539 000003ae
 ckstatsum ddf65d49 00000413
-formfile 68be3ca9 00000f51
+formfile 03983008 00000f5c
 formfilestats fa92ede0 000004dc
 revargs 5db3f9bb 000000a7
 spin2 1263edf2 00000180
@@ -42,11 +42,11 @@ ps | grep -E "^[ ]*$$" | grep -q bash   || chkexit "$0 : Not bash"
 test -d "$links/"                       || chkexit "$0 : not a directory links='$links'"
 
 f="$0"
-infile="${0##*/}"                                              # infile  == basename 0
-expr "$f" : ".*/" >/dev/null && inpath="${f%/*}" || inpath="." # inpath  ==  dirname 0
+infile="${f##*/}"                                              # infile  == basename f
+expr "$f" : ".*/" >/dev/null && inpath="${f%/*}" || inpath="." # inpath  ==  dirname f
 infilep="$(cd "${inpath}" ; pwd -P)/${infile}"                 # infilep == realpath f
 
-gen_index () { # for "$links/$name/"
+gen_index () { # in pwd, for "$links/$name/"
     mkdir -p html
 
     chktrue "${name}.list"
@@ -62,14 +62,14 @@ gen_index () { # for "$links/$name/"
     touch -r "$links/$name/" "${name}.list"
 
     chktrue ${name}.tab
-    local j="/tmp/${FUNCNAME}-$$/second"
+    local j="/tmp/${FUNCNAME}-$$/2nd/3rd"
     mkdir -p "$j"
     cat ${name}.list | while IFS= read a ; do
         b="$(cd "$j" ; formfile "$a" | sed '/^#/d')"
         sed -e 's/   .*_^/ _^/' -e "s/'//" -e 's/^\(.*\)\( _^.*\)/\2 \1/' -e 's/^\(.*\),\([^ ]*\)/\2 \1 /' <<<"$b" \
             | awk '{printf "%-85s %-18s",$1,$2;$1="";$2="";$3="";print}'
         spin2
-        done | sort -f >${name}.tab
+        done | sort -f >${name}.tab~ && mv ${name}.tab~ ${name}.tab
     spin2 0
     rm -rf "/tmp/${FUNCNAME}-$$/"
     touch -r "${name}.list" ${name}.tab
@@ -85,8 +85,8 @@ gen_index () { # for "$links/$name/"
 
     chktrue ${name}.ckstat
     cat ${name}.list | while IFS= read a ; do
-        ckstat    $links/$name/$a | awk -v f="${a##*,}" '{printf ". % 8s % 8s %s %s\n",$2,$3,$4,f}'
-      # ckstatsum $links/$name/$a | awk -v f="${a##*,}" '{printf ". % 8s % 8s %s %s\n",$2,$3,$4,f}'
+        ckstat    $links/$name/$a | awk -v f="${a##*,}" '{printf ". % 8s % 8s %s %s\n",$3,$4,$5,f}'
+      # ckstatsum $links/$name/$a | awk -v f="${a##*,}" '{printf ". % 8s % 8s %s %s\n",$3,$4,$5,f}'
         spin2
         done >${name}.ckstat
     spin2 0
@@ -96,6 +96,8 @@ gen_index () { # for "$links/$name/"
     rm -rf   "$links/0/kind/$name"
     mkdir -p "$links/0/kind/$name"
     ln "$links/$name/"*mp3 "$links/0/kind/$name"
+    # purge staging (0*) and offramp (y*) mp3
+    rm -f "$links/0/kind/$name/"0*mp3 "$links/0/kind/$name/"y*mp3
     touch -r "$links/$name" "$links/0/kind/$name"
 
     mkdir -p html
@@ -144,14 +146,18 @@ check_do_index () { # bypass unchanged
 touch_kind_rsync () {
     mount | grep -q /Volumes/CURATE || { chkwrn "$0 : no /Volumes/CURATE for kind/$name" ; return 1 ;}
     mkdir -p "/Volumes/CURATE/kind/$name" || { chkwrn "$0 : cannot create /Volumes/CURATE/kind/$name" ; return 1 ;}
-    # Fixup OSX Ventura rsync problem
+    # Fixup OSX Ventura rsync problem (sets timestamp to transfer time vs orig file time, to ms-dos filesystems)
     # https://discussions.apple.com/thread/254383328
     # https://github.com/WayneD/rsync/issues/412
+    # before this workaround, other (recursive) cases will require directory creation too...
+         # mkdir -p $(dirname "/Volumes/CURATE/kind/${name}/$a"
     cd "$links/0/kind/$name/" \
-      && find . -type f -maxdepth 1 -name \*\.mp3 | while IFS= read a ; do
-        touch -r "$a" "/Volumes/CURATE/kind/${name}/$a"
-        done
-    rsync -aPi --delete --delete-excluded --modify-window=1 --include='*mp3' --exclude='*' ./ "/Volumes/CURATE/kind/$name/"
+      && {
+        find . -type f -maxdepth 1 -name \*\.mp3 | while IFS= read a ; do
+            touch -r "$a" "/Volumes/CURATE/kind/${name}/$a"
+            done
+        rsync -aP --delete --delete-excluded --modify-window=1 --include='*mp3' --exclude='*' ./ "/Volumes/CURATE/kind/$name/"
+        }
     }
 
 # main
@@ -172,6 +178,7 @@ volumes="
 5d50-kindle-class https://github.com/georgalis/pub/blob/master/know/music/5d50-kindle-class.list
 6344-Ithica       https://github.com/georgalis/pub/blob/master/know/music/6344-Ithica.list
 6350-forte-flute  https://github.com/georgalis/pub/blob/master/know/music/6350-forte-flute.list
+63aa-bee-piano    https://github.com/georgalis/pub/blob/master/know/music/63aa-bee-piano.list
 "
 
 vols="$(awk '!/^$/ {print $1}' <<<"$volumes")"
@@ -182,7 +189,7 @@ awk '!/^$/' <<<"$volumes" | awk '{print NR,$0}'
 cd "$inpath"
 
 # if the first arg is sync, do that after gen_index
-[ "$1" = "sync" ] && { _sync=y ; shift ;} || { _sync=n ;;}
+[ "$1" = "sync" ] && { _sync=y ; shift ;} || { _sync=n ;}
 
 # always read args
 [ "$*" ] && { for name in $* ; do [ -d "$links/$name/" ] && gen_index ; done ;} \
